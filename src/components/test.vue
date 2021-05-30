40f4929244fcf95401c4034c4cc324e2e912b051
ViavaCos <13811092694@163.com>
2021-05-30 19:03:31 +0800

    新增前端基础库

---
+++ src/components/test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h2>计数：{{  count }}</h2>
+    <test-button @click="test">测试</test-button>
+  </div>
+</template>
+
+<script>
+import { test  } from 'submodule/utils'
+import { testButton } from 'submodule/components'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    testButton
+    // 'testButton': () => import('submodule/components')
+  },
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      count: 0
+    }
+  },
+  methods:{
+    test(){
+      this.count = test(this.count, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// 引入前段基础库，然后可以做些初始化啥的
+import Submodule from 'submodule'
+const sub = new Submodule()
+
+console.log(sub);
+
 createApp(App).mount('#app')
